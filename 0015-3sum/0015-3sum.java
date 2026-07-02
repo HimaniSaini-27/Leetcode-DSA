321f6a3b377763dcc00bbf6b2f5d955e7d7257d5
HimaniSaini-27 <himanisaini1787@gmail.com>
2026-07-02 14:38:42 +0530

    Time: 688 ms (11.3%), Space: 60.7 MB (5.84%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,48 +1,31 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
         Arrays.sort(nums);
 
-        List<List<Integer>> result = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        Set<List<Integer>> seen = new HashSet<>();
 
-        for (int i = 0; i < nums.length - 2; i++) {
-
-            if (i > 0 && nums[i] == nums[i - 1])
-                continue;
-
-            int left = i + 1;
-            int right = nums.length - 1;
-
-            while (left < right) {
-
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if (sum == 0) {
-
-                    result.add(Arrays.asList(
-                            nums[i],
-                            nums[left],
-                            nums[right]
-                    ));
-
-                    left++;
-                    right--;
-
-                    while (left < right &&
-                           nums[left] == nums[left - 1])
-                        left++;
-
-                    while (left < right &&
-                           nums[right] == nums[right + 1])
-                        right--;
-
-                } else if (sum < 0) {
-                    left++;
-                } else {
-                    right--;
+        for (int i = 0; i < n; i++) {
+            int target = nums[i];
+            int x = i+1, y = n-1;
+            while (x < y) {
+                int sum = nums[x] + nums[y];
+                if (sum < (-1 * target)) x++;
+                else if (sum > (-1 * target)) y--;
+                else {
+                    List<Integer> list = new ArrayList<>(Arrays.asList(target, nums[x], nums[y]));
+                    Collections.sort(list);
+                    if (!seen.contains(list)) {
+                        seen.add(list);
+                        ans.add(list);
+                    }
+                    x++;
+                    y--;
                 }
             }
         }
-
-        return result;
+        
+        return ans;
     }
 }
